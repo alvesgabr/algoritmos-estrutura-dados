ad91da5b18d4bc68a8db010a38d6c728e600d03a
João Gabriel Alves <joao.alves.1129911@sga.pucminas.br>
2022-09-14 11:27:48 -0300

    linkedlist main command tests

--- exercises/U2/REFCL01.cs
+++ exercises/U2/REFCL01.cs
@@ -30,12 +30,12 @@ class List
 // insert at the next 'node', & for addEnd, change pointer pos
   public void AddStart(int n)
   {
-    Node temp = new Node(n); // new node of data n
-    temp.next = _head.next;    // next pointer of node temp = next pointer of initial node
-    _head.next = temp;         // next pointer ini node = node temp (not pointer!)
+    Node temp = new Node(n);     // new node of data n
+    temp.next = _head.next;      // next pointer of node temp = next pointer of initial node
+    _head.next = temp;           // next pointer ini node = node temp (not pointer!)
     if(_head == _tail)           // if initial and last node pointers are the same:
-      _tail = temp;            // last node pointer moves to temp node
-    temp = null;             // nullify temp for clean up
+      _tail = temp;              // last node pointer moves to temp node
+    temp = null;                 // nullify temp for clean up
   }
 
   public void AddEnd(int n)
@@ -53,10 +53,10 @@ class List
     }
 
     Node temp = _head;
-    _head = _head.next; // moving initial pointer
-    int tdata = _head.data; // assign new initial pointer data to temp var
-    temp = temp.next = null; // discard temp
-    return tdata; // return value
+    _head = _head.next;       // moving initial pointer
+    int tdata = _head.data;   // assign new initial pointer data to temp var
+    temp = temp.next = null;  // discard temp
+    return tdata;             // return value
   }
 // Task 1 - Modify RemoveStartLog to remove the first node phisically, I have 2 solutions below, A and B
 
@@ -70,11 +70,11 @@ class List
     }
 
     Node temp = _head;
-    _head = _head.next; // moving initial pointer
-    int tdata = _head.data; // assign new initial pointer data to temp var
-    temp = temp.next = null; // discard temp
-    _head.data = 0; // declaring 0 to first node data
-    return tdata; // return value
+    _head = _head.next;       // moving initial pointer
+    int tdata = _head.data;   // assign new initial pointer data to temp var
+    temp = temp.next = null;  // discard temp
+    _head.data = 0;           // declaring 0 to first node data
+    return tdata;             // return value
   }
 
 // B solution - declares a new node and copies the past first node reference and discard everything behind.
@@ -85,13 +85,13 @@ class List
       Console.WriteLine("Invalid Command - ERR001: First and Last Node are the same!");
       Environment.Exit(0);
     }
-    Node temp = new Node(); // declare new node
-    temp.next = _head.next.next; // assign pointers for temp as third node
-    int tdata = _head.next.data; // store returning data
+    Node temp = new Node();              // declare new node
+    temp.next = _head.next.next;         // assign pointers for temp as third node
+    int tdata = _head.next.data;         // store returning data
     _head.next = _head.next.next = null; // nullyfing nodes to be delloc
-    _head = temp; // moving pointers
-    temp = temp.next = null; // nullyfing nodes to be delloc
-    return tdata; // return value
+    _head = temp;                        // moving pointers
+    temp = temp.next = null;             // nullyfing nodes to be delloc
+    return tdata;                        // return value
   }
 
 // Conclusion: Solution B is unefficient
@@ -120,13 +120,13 @@ class List
       Environment.Exit(0);
     }
     
-    Node temp; // new temporary node
+    Node temp;                // new temporary node
     // running thru the list node:node and assign temp as Last Node
     for(temp = _head; temp.next != _tail; temp = temp.next);
-    int tdata = _tail.data; // assign last node data to temp data
-    _tail = temp;          // last node points to previous node (temp node pointer)
+    int tdata = _tail.data;   // assign last node data to temp data
+    _tail = temp;             // last node points to previous node (temp node pointer)
     temp = _tail.next = null; // tempnode and last next node pointers nullyfied
-    return tdata; // return data stored at temp data from last node data, now nullyfied
+    return tdata;             // return data stored at temp data, now nullyfied
   }
 
   public void AddPosition(int n, int pos)
@@ -147,16 +147,17 @@ class List
     } 
     else
     {
-    Node temp = _head; // mark temp pointer to first node
-    for(int i = 0; i < pos; i++, temp = temp.next); // run thru array and moving temp pointer along with counter
+    Node temp = _head;        // mark temp pointer to first node
+    // run thru array and moving temp pointer along with counter
+    for(int i = 0; i < pos; i++, temp = temp.next); 
     Node stemp = new Node(n); // declare new node with the desired value
-    stemp.next = temp.next; // connect new node to pos + 1
-    temp.next = stemp; // connect pos node to new node
-    stemp = temp = null; // nullyfing both temps
+    stemp.next = temp.next;   // connect new node to pos + 1
+    temp.next = stemp;        // connect pos node to new node
+    stemp = temp = null;      // nullyfing both temps
     }
   }
   
-  public int RemovePosition(int pos)
+  /*public int RemovePosition(int pos)
   {
     int tdata, size = ListSize();
     if(_head == _tail || pos < 0 || pos >= size)
@@ -183,7 +184,7 @@ class List
       n = temp = null;
     }
     return tdata;
-  }
+  }*/
 
   public void Mostrar()
   {
@@ -209,15 +210,39 @@ class REFCL01
   static void Main(string[] args)
   {
     List list = new List();
-    for(int i = 0; i < 10; i++)
+    for(int i = 1; i <= 10; i++)
     {
       list.AddEnd(i);
     }
-    list.AddStart(10);
+    list.AddStart(11);
+    list.AddStart(12);
     list.AddPosition(20, 2);
-    list.Add
+    list.AddEnd(99);
     
+    list.Mostrar();
+    Console.WriteLine();
+    
+    int logicallyRemoved = list.RemoveStartLog();
+    int professorRemoved = list.RemoveStartProfessor();
+    int physicallyRemovedA = list.RemoveStartPhysA();
+    int physicallyRemovedB = list.RemoveStartPhysB();
+    
+    Console.WriteLine($"\nHead Logically Removed: {logicallyRemoved}");
+    Console.WriteLine($"Professor Removed: {professorRemoved}");
+    Console.WriteLine($"Head Phsyically Removed A: {physicallyRemovedA}");
+    Console.WriteLine($"Head Phsyically Removed B: {physicallyRemovedB}");
+    
+    /*
+    Not functional methods:
 
-    Console.Write($"{testa.data} <- Teste A\n{testb.data} <- Teste B\n");
+    Remove At Postion pos
+    int pos = 2;
+    int posRemove = list.RemovePosition(pos);
+    Console.WriteLine($"Position {pos} Removed:\n{posRemove}");
+
+    Remove End
+    int tailRemove = list.RemoveEnd();
+    Console.WriteLine($"End of List Removed: {tailRemove}");
+    */
   }
 }
